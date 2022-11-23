aab0f43404946f7280c20129d56077a86bbe9ac4
RedEye75 <yusufbinemamstk@gmail.com>
2022-11-24 02:02:57 +0600

    aos animation added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,6 +14,8 @@ const Home = () => {
                   <div className="absolute rounded-full bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
                   <div className="relative">
                     <img
+                      data-aos="fade-right"
+                      data-aos-duration="3000"
                       className="object-cover object-center mx-auto rounded-lg shadow-2xl"
                       alt="hero"
                       src="https://img.freepik.com/free-photo/portrait-smiling-young-girl-using-mobile-phone_171337-1627.jpg?w=1060&t=st=1669224128~exp=1669224728~hmac=2bc8f03b3ca912c13bd2f20ee8408fcf661a4ed4a06ce5f93538e6dee137675d"
@@ -22,7 +24,11 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="3000"
+              className="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0"
+            >
               <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase">
                 {" "}
                 Hello sir..
