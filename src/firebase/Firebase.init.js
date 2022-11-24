937042c33da84f4c371595e05a95a3eaf020beba
RedEye75 <yusufbinemamstk@gmail.com>
2022-11-24 17:39:41 +0600

    firebase onfig added

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAy2fdbCcbDY7gHGgnzsQTDnjQyLS-iKAE",
+  authDomain: "smartzone-bb172.firebaseapp.com",
+  projectId: "smartzone-bb172",
+  storageBucket: "smartzone-bb172.appspot.com",
+  messagingSenderId: "855469195076",
+  appId: "1:855469195076:web:38d10bdb9052442c68a111",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
